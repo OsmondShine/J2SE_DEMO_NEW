344a4aa81a126216267d89169fffe8a7fef2ae87
Felix <760311677@qq.com>
2017-10-30 21:14:20 +0800

    Update BinaryTree.java

--- src/com/xl/datastruts/binarytree/BinaryTree.java
+++ src/com/xl/datastruts/binarytree/BinaryTree.java
@@ -1,69 +1,41 @@
 package com.xl.datastruts.binarytree;
 
-import java.lang.reflect.Method;
-
 /**
  * @author 作者:xl
- * @data   创建时间:2017-6-19
+ * @data   创建时间:2017-10-28
  * @description 
  * @version 1.0
  */
 
-public interface BinaryTree {
-	/**
-     * 判断二叉树是否为空
-     * @return 二叉树为空则返回true，否则返回false
-     */ 
-    public boolean isEmpty(); 
- 
-    /**
-     * 返回二叉树的根
-     * @return 二叉树的根
-     */ 
-    public Object root(); 
- 
-    /**
-     * 指定根，左孩子，右孩子创建二叉树
-     * @param root 根
-     * @param left 做孩子
-     * @param right 右孩子
-     */ 
-    public void makeTree(Object root, Object left, Object right); 
- 
-    /**
-     * 删除二叉树的左子树并返回
-     * @return 二叉树的左子树
-     */ 
-    public BinaryTree removeLeftSubtree(); 
- 
-    /**
-     * 删除二叉树的右子树并返回
-     * @return 二叉树的右子树
-     */ 
-    public BinaryTree removeRightSubtree(); 
- 
-    /**
-     * 使用指定的节点访问方法前序遍历二叉树
-     * @param visit 指定的节点访问方法
-     */ 
-    public void preOrder(Method visit); 
- 
-    /**
-     * 使用指定的节点访问方法中序遍历二叉树
-     * @param visit 指定的节点访问方法
-     */ 
-    public void inOrder(Method visit); 
- 
-    /**
-     * 使用指定的节点访问方法后序遍历二叉树
-     * @param visit 指定的节点访问方法
-     */ 
-    public void postOrder(Method visit); 
- 
-    /**
-     * 使用指定的节点访问方法层序遍历二叉树
-     * @param visit 指定的节点访问方法
-     */ 
-    public void levelOrder(Method visit); 
+public class Node {
+	private int data;
+	private Node leftNode;
+	private Node rightNode;
+
+	public Node(){}
+	public Node(int data, Node leftNode, Node rightNode) {
+		super();
+		this.data = data;
+		this.leftNode = leftNode;
+		this.rightNode = rightNode;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public Node getLeftNode() {
+		return leftNode;
+	}
+	public void setLeftNode(Node leftNode) {
+		this.leftNode = leftNode;
+	}
+	public Node getRightNode() {
+		return rightNode;
+	}
+	public void setRightNode(Node rightNode) {
+		this.rightNode = rightNode;
+	}
 }
 
